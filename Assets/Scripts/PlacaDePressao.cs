f7b504fa17951449866d98f70baea313e00bbf85
Higor V.L <higor-lopo@hotmail.com>
2023-11-16 19:04:59 -0400

    Bugfix/Box_Collider_do_Item_Caixa
    correção do bug de collider do gameobject caixa do puzzle do mapa 2.

--- Assets/Scripts/PlacaDePressao.cs
+++ Assets/Scripts/PlacaDePressao.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class PlacaDePressao : MonoBehaviour{
 
     private static int placasAtivadas = 0;
-    void Update(){
-        Debug.Log("placas ativadas"+placasAtivadas);
-    }
 
 
     private void OnTriggerEnter(Collider other){
-        if(placasAtivadas<2){
-            placasAtivadas++;
-        }
-        
+        placasAtivadas++;
         if (placasAtivadas == 2){
             GlobalVariables.checaPortaAtiva = true;
             Debug.Log("Placa de pressao ativada!");
@@ -22,10 +16,7 @@ public class PlacaDePressao : MonoBehaviour{
     }
 
     private void OnTriggerExit(Collider other){  
-        if(placasAtivadas>0){
-            placasAtivadas--;
-        }
-        
+        placasAtivadas--;
         if (placasAtivadas < 2){
             GlobalVariables.checaPortaAtiva = false;
             Debug.Log("Placa de pressao desativada!");
